388f34d1de0f92df65b25e15496d28b76338c950
Hiep Tran <hoanghieptranj5@gmail.com>
2021-05-09 18:04:20 +0700

    Added FolderBuilder

--- StockMaster/Program.cs
+++ StockMaster/Program.cs
@@ -4,17 +4,27 @@ using StockMaster.Analysis;
 using StockMaster.Minions;
 using StockMaster.Minions.CoPhieu68;
 using StockMaster.Services.Files;
+using StockMaster.Services.FolderServices;
 
 namespace StockMaster
 {
     class Program
     {
+        /// <summary>
+        /// DO NOT DELETE
+        /// this function setup all available resources
+        /// </summary>
+        private static void StartUp()
+        {
+            new FolderBuilder();
+        }
+
         static void Main(string[] args)
         {
+            StartUp();
 
-
-            var minion = new VnDirectMinion();
-            minion.Execute();
+            //var minion = new VnDirectMinion();
+            //minion.Execute();
 
             //StockFinder.CompareCurrentPriceWithRecommendedPrice();
         }
---
+++ StockMaster/Services/FolderServices/FolderBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StockMaster.Services.FolderServices
+{
+    public class FolderBuilder
+    {
+        public FolderBuilder()
+        {
+            var folders = new string[] { "resources", "data" };
+            MakeFolders(folders);
+        }
+
+        private void MakeFolders(string[] folders)
+        {
+            foreach (var folder in folders)
+            {
+                System.IO.Directory.CreateDirectory(Environment.CurrentDirectory + "/" + folder);
+            }
+        }
+    }
+}
