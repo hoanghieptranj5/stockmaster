dad0cbf1919cf25380ebb4d00d9e3280c7e3a425
Hiep Tran <hoanghieptranj5@gmail.com>
2021-07-04 12:26:29 +0700

    refactored code

--- Cp68Minion/Cp68StockInfoMinion.cs
+++ Cp68Minion/Cp68StockInfoMinion.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Cp68Minion.Models;
+using Cp68Minion.Utils;
 using StockMaster.Contracts;
+using StockMaster.Minions.Xpaths.CoPhieu68;
 using StockMaster.Models.CoPhieu69;
+using StockMaster.Services.Converts;
 using StockMaster.Services.Files;
 
 namespace Cp68Minion
@@ -25,6 +28,51 @@ namespace Cp68Minion
             };
             
             throw new NotImplementedException();
+        }
+        
+        public List<CompanyListModel> GetAll()
+        {
+            var result = new List<CompanyListModel>();
+
+            foreach (var stc in Stc.StcIds)
+            {
+                var tempoList = new CompanyListModel();
+                
+                var companiesByStc = new List<Company>();
+                for (var page = 1; page <= 16; page++)
+                {
+                    SeleniumService.GoTo(CoPhieu68Xpath.GetCompanyListUrl(stc.Key, page));
+
+                    var rows = SeleniumService.FindElementsByXpath(CoPhieu68Xpath.CompanyTableRowXpath);
+                    foreach (var row in rows)
+                    {
+                        var innerTds = SeleniumService.FindInnerElementsByXpath(row, ".//td");
+                        companiesByStc.Add(new Company
+                        {
+                            Id = innerTds[1].Text,
+                            Name = innerTds[2].Text,
+                            FirstDate = StringToDateTimeConverter.Convert(innerTds[3].Text, "dd/mm/yyyy"),
+                            FirstTimeVolume = StringToNumberConverter.ConvertToDouble(innerTds[4].Text),
+                            FirstPrice = StringToNumberConverter.ConvertToDouble(innerTds[5].Text),
+                            CurrentVolume = StringToNumberConverter.ConvertToDouble(innerTds[6].Text),
+                            TreasuryShares = StringToNumberConverter.ConvertToDouble(innerTds[7].Text),
+                            ListedVolume = StringToNumberConverter.ConvertToDouble(innerTds[8].Text),
+                            ForeignOwn = StringToNumberConverter.ConvertToDouble(innerTds[9].Text),
+                            ForeignAllowedToBuy = StringToNumberConverter.ConvertToDouble(innerTds[10].Text),
+                            Price = StringToNumberConverter.ConvertToDouble(StringUtils.RemoveBraces(innerTds[11].Text)),
+                            MarketCap = StringToNumberConverter.ConvertToDouble(innerTds[12].Text),
+                            Chart = string.Empty
+                        });
+                    }
+                }
+
+                tempoList.Stc = stc;
+                tempoList.Companies = companiesByStc;
+                
+                result.Add(tempoList);
+            }
+
+            return result;
         }
     }
 }
---
+++ Cp68Minion/Logic/CompanyListHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cp68Minion.Models;
+using StockMaster.Minions.Xpaths.CoPhieu68;
+using StockMaster.Models.CoPhieu69;
+using StockMaster.Services.Selenium;
+
+namespace Cp68Minion.Logic
+{
+    public class CompanyListHandler
+    {
+        
+    }
+}
