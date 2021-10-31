ed4440676feb98b045992910350cb3d2029e91cf
Hiep Tran <hoanghieptranj5@gmail.com>
2021-10-31 16:19:23 +0700

    refactored code

--- StockMaster/Services/AppLogic.cs
+++ StockMaster/Services/AppLogic.cs
@@ -1,29 +1,21 @@
-using System.Collections.Generic;
 using StockMaster.Analysis;
 using StockMaster.Contracts;
 using StockMaster.Minions;
 using StockMaster.Minions.CoPhieu68;
-using StockMaster.Services.Files;
-using StockMaster.Services.Logger;
 
 namespace StockMaster.Services
 {
     public class AppLogic : IAppLogic
     {
-        private readonly ILoggerService _logger;
-        private readonly FileService _fileService;
         private readonly StockFinder _stockFinder;
         private readonly GetRecommendMinion _getRecommendMinion;
         private readonly StockInfoMinion _stockInfoMinion;
 
-        public AppLogic(ILoggerService logger, 
-            FileService fileService, 
+        public AppLogic( 
             StockFinder stockFinder, 
             GetRecommendMinion getRecommendMinion, 
             StockInfoMinion stockInfoMinion)
         {
-            _logger = logger;
-            _fileService = fileService;
             _stockFinder = stockFinder;
             _getRecommendMinion = getRecommendMinion;
             _stockInfoMinion = stockInfoMinion;
